d432b8e5aa27dd30ad2dfa95f62020240d265e15
Rendy Septian Pradana <126131312+Rendyseptch@users.noreply.github.com>
2025-06-23 21:35:41 +0700

    rename class

--- app/View/Components/serviceSection.php
+++ app/View/Components/serviceSection.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class serviceSection extends Component
+class ServiceSection extends Component
 {
     /**
      * Create a new component instance.
